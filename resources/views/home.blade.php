29113b16502b9068b303b8c77f4a3af478955c12
Marco Tulio Ramos Filho <tulio@mechworks.com.br>
2020-05-04 21:32:33 -0300

    materialize

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,18 @@
 @section('content')
 {{-- List all posts --}}
 <div class="row mx-auto d-flex justify-content-center">
-    <div class="card-columns justify-content-center">
-        @forelse ($posts as $post)
-        <div class="card border-primary mx-auto" style="max-width: 18rem;">
-            <div class="card-body ">
-                <p class="card-text">{{ $post->body }}</p>
-            </div>
+    @forelse ($posts as $post)
+    <div class="col s12 m5">
+        <div class="card blue-grey darken-1" style="max-width: 18rem;">
+
+                <span class="white-text word-wrapnp">{{ $post->body }}</span>
+
             <small class="card-footer bg-transparent float-left text-muted border-white">{{ $post->created_at->diffForHumans() }}</small>
         </div>
-        @empty
-        <p>sem surtos ainda!</p>
-        @endforelse
     </div>
+    @empty
+    <p>sem surtos ainda!</p>
+    @endforelse
 </div>
 
 
