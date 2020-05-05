b6200ecfbc4df644ea06604e32f36e579c2fcfd6
Marco Tulio Ramos Filho <tulio@mechworks.com.br>
2020-05-05 16:39:56 -0300

    master

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,11 @@
 <div class="row mx-auto justify-content-center text-center" style="width:100%;">
     <div class="card-columns">
         @forelse ($posts as $post)
-        <div class="card text-white bg-primary text-left" style="max-width: 18rem;">
+        <div class="card border-info text-left" style="max-width: 18rem;">
             <div class="card-body">
                 <p class="card-text">{{ $post->body }}</p>
             </div>
-            <small class="card-footer bg-transparent float-left text-muted-white border-primary">{{ $post->created_at->diffForHumans() }}</small>
+            <small class="card-footer bg-transparent float-left text-muted-white border-white" style="width:100%;">{{ $post->created_at->diffForHumans() }}</small>
         </div>
         @empty
         <p>sem surtos ainda!</p>
