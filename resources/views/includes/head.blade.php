cde85c398c4240f1d7b1f89f0b14b8b3590c93e5
Marco Tulio Ramos Filho <tulio@mechworks.com.br>
2020-05-04 22:14:49 -0300

    master

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -5,9 +5,4 @@
 <title>mama surta</title>
 <!-- Bootstrap core CSS -->
 <!-- Custom styles for this template -->
-
-<!-- Compiled and minified CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
-<!-- Compiled and minified JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<link href="/css/app.css" rel="stylesheet">
