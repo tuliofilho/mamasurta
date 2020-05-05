d5b80ef35761f58bac74662f792c2d4125fbf7c7
Marco Tulio Ramos Filho <tulio@mechworks.com.br>
2020-05-05 19:55:12 -0300

    master

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,4 +1,4 @@
-<div class="jumbotron float-md-center" style="width:100%;">
+<div class="jumbotron mt-3" style="width:100%;">
     <div class="container-fluid">
         <h2 class="display-4">mama surta</h2>
         <p class="lead">mural de confidências maternas anônimas</p>
